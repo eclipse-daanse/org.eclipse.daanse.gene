1cf59037a178fa8cb73ba0444847d8dd6a54ac04
vwfox <markus.hochstein@software-hochstein.de>
2026-03-22 18:10:03 +0100

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+/**
+Copyright (c) 2025 Contributors to the  Eclipse Foundation.
+This program and the accompanying materials are made
+available under the terms of the Eclipse Public License 2.0
+which is available at https://www.eclipse.org/legal/epl-2.0/
+SPDX-License-Identifier: EPL-2.0
+
+Contributors: Smart City Jena
+*/
+
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/'],
+  },
+]
